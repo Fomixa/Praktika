73bce5350ac94a5e82aea7984bf62a98efe0e714
Fomixa </>
2020-11-06 13:42:33 +0300

    wow, again smth new

---
+++ praktika2/praktikum2/FileDemo2/FileDemo2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace FileDemo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      StreamWriter writer = File.CreateText(@"C:\Users\student\source\repos\praktika\praktika2\praktikum2\FileDemo2\text.txt"); writer.WriteLine("This is my new file");
+      writer.WriteLine("Do you like its format?"); writer.Close();
+      StreamReader reader = File.OpenText(@"C:\Users\student\source\repos\praktika\praktika2\praktikum2\FileDemo2\text.txt");
+      string contents = reader.ReadToEnd();
+      reader.Close();
+      Console.WriteLine(contents);
+    }
+  }
+}
