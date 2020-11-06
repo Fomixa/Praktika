bb32f4a8d39c6b468df54690340e32cbd60d2cd8
Fomixa </>
2020-11-06 13:38:56 +0300

    robit

---
+++ praktika2/praktikum2/FileDemo/FileDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace FileDemo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      StreamWriter writer = File.CreateText(@"C:\Users\student\source\repos\praktika\praktika2\praktikum2\FileDemo\text.txt"); writer.WriteLine("This is my new file");
+      writer.WriteLine("Do you like its format?"); writer.Close();
+    }
+  }
+}
