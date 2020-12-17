4f3a76434efc6dc7dada0547ba3f26aafb4c7dde
Андрей Фомин <aa-fomin2@yandex.ru>
2020-12-17 08:08:49 +0300

    1223415

--- praktika6/ConsoleApp1/ConsoleApp1/Program.cs
+++ praktika6/ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,11 +8,85 @@ namespace practica_6
     {
         static void Main(string[] args)
         {
-            int nrow, ncolumn;
-            Console.WriteLine("Введите кол-во строк и столбцов через пробел");
-            string line = Console.ReadLine();
-            var arr = line.Split(' ');
+            ResearchTeam team = new ResearchTeam();
+            Console.WriteLine("Ту шорт стринг)) " + team.ToShortString());
+            Console.WriteLine("Индекс год " + team[TimeFrame.Year]);
+            Console.WriteLine("Индекс два года " + team[TimeFrame.TwoYears]);
+            Console.WriteLine("Индекс давно " + team[TimeFrame.Long]);
+            team.Theme_of_res = "Dota 2";
+            team.Name_of_org = "Valve";
+            team.Number_of_reg = 222222228;
+            team.duration = TimeFrame.Year;
+            team._publications.Add(new Paper());
+            Console.WriteLine("Данные объекта " + team.ToString());
+            team.AddPapers(new Paper("Pudge", new Person("Gabe", "Newell", new DateTime(1963, 7, 20)), new DateTime(2020, 10, 10)),
+                           new Paper("Sven", new Person("Gaben", "Newellich", new DateTime(1961, 8, 18)), new DateTime(2020, 12, 9)));
+            Console.WriteLine("Данные объекта 2 " + team.ToString());
+            Console.WriteLine("Последняя публикация " + team.LastPublic.ToString());
 
+            int start, end, time;
+            Paper one = new Paper("Pudge", new Person("Gabe", "ewell", new DateTime(1963, 7, 20)), new DateTime(2020, 10, 10));
+            Paper two = new Paper("Sven", new Person("Gaben", "Nwellich", new DateTime(1961, 8, 28)), new DateTime(2020, 10, 10));
+            Paper three = new Paper("Sve", new Person("Gben", "Newellih", new DateTime(1661, 8, 18)), new DateTime(2020, 12, 09));
+            Paper four = new Paper("Svn", new Person("aben", "Neellich", new DateTime(1971, 8, 3)), new DateTime(2020, 12, 09));
+            Paper five = new Paper("ven", new Person("Gabn", "Newelich", new DateTime(1261, 8, 8)), new DateTime(2020, 12, 09));
+            Paper six = new Paper("Sen", new Person("Gaen", "Newelich", new DateTime(1963, 8, 28)), new DateTime(2020, 12, 09));
+            Paper[] linear = new Paper[] { one, two, three, four, five, six };
+            Paper[,] matrix = new Paper[2, 3] { { one, two, three }, { four, five, six } };
+            Paper[][] rank = new Paper[3][];
+            rank[0] = new Paper[1] { one };
+            rank[1] = new Paper[2] { two, three };
+            rank[2] = new Paper[3] { four, five, six };
+
+            start = Environment.TickCount;
+            for (int i = 0; i < linear.Length; i++)
+            {
+                for (int j = 0; j < 1000000; j++)
+                {
+                    linear[i].ToString();
+                    linear[i].Author.ToShortString();
+                    linear[i].NameP.ToString();
+                }
+            }
+            end = Environment.TickCount;
+            time = end - start;
+            Console.WriteLine("Время для линейного массива: " + time);
+
+
+            start = Environment.TickCount;
+            for (int i = 0; i < rank.Length; i++)
+            {
+                for (int j = 0; j < rank[i].Length; j++)
+                {
+                    for (int k = 0; k < 1000000; k++)
+                    {
+                        rank[i][j].ToString();
+                        rank[i][j].Author.ToShortString();
+                        rank[i][j].NameP.ToString();
+                    }
+                }
+            }
+            end = Environment.TickCount;
+            time = end - start;
+            Console.WriteLine("Время для зубчатого массива: " + time);
+
+            start = Environment.TickCount;
+            for (int i = 0; i < matrix.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < matrix.Length / (matrix.GetUpperBound(0) + 1); j++)
+                {
+                    for (int k = 0; k < 1000000; k++)
+                    {
+                        matrix[i, j].ToString();
+                        matrix[i, j].Author.ToShortString();
+                        matrix[i, j].NameP.ToString();
+                    }
+                }
+
+            }
+            end = Environment.TickCount;
+            time = end - start;
+            Console.WriteLine("Время для матрицы: " + time);
         }
     }
 
@@ -240,3 +314,4 @@ namespace practica_6
     }
 
 }
+
